5c81ddd4863f5ffcd8d0f1301c61519b6643bce0
wannabecoder1729 <rudrasenj1729@gmail.com>
2026-09-09 17:53:05 +0530

    Create __init__.py

---
+++ EmotionDetection/__init__.py
@@ -0,0 +1,8 @@
+"""
+EmotionDetection package initializer.
+
+Importing from this file makes emotion_detector directly accessible via:
+    from EmotionDetection import emotion_detector
+"""
+
+from EmotionDetection.emotion_detection import emotion_detector
